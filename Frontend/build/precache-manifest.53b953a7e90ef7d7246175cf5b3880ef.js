2082cb29ebf3b77cdf5cf7516ed4ecb91f3d4702
manoji <mahamano1965@gmail.com>
2020-04-01 20:51:33 +0530

    First commit for Deployement

---
+++ Frontend/build/precache-manifest.53b953a7e90ef7d7246175cf5b3880ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d341db33d55950b6386e9d7cfa4899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef332e0d33c1f537f63",
+    "url": "/static/css/2.aef2094a.chunk.css"
+  },
+  {
+    "revision": "397f970276a44c6177f4",
+    "url": "/static/css/main.904add86.chunk.css"
+  },
+  {
+    "revision": "eef332e0d33c1f537f63",
+    "url": "/static/js/2.86cf1dc5.chunk.js"
+  },
+  {
+    "revision": "74dcd8052a644434877c2eb155510a9e",
+    "url": "/static/js/2.86cf1dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397f970276a44c6177f4",
+    "url": "/static/js/main.d0f733bf.chunk.js"
+  },
+  {
+    "revision": "45c8a245a3583d2d72da",
+    "url": "/static/js/runtime-main.ef16c1c7.js"
+  }
+]);
